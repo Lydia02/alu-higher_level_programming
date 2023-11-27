a7cde69785655224da169f955c1283c2c833a8e5
Lydia <l.ojoawo@alustudent.com>
2023-11-27 20:12:25 +0000

    task done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status"""
+'''a Python script that fetches a url'''
+
 
 import urllib.request
-from urllib.error import HTTPError
+
 
 if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    
-    try:
-        with urllib.request.urlopen(url) as response:
-            content = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode("utf-8")))
-    except HTTPError as e:
-        if e.code == 403:
-            print("Body response:")
-            print("\t- type: <class 'bytes'>")
-            print("\t- content: b'Custom status'")
-            print("\t- utf8 content: Custom status")
-        else:
-            raise e
-
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
