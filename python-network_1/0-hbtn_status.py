df2dc65d4579eaa9a10879d7da2ba4234d91d41f
Lydia <l.ojoawo@alustudent.com>
2023-11-27 20:33:31 +0000

    task done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-'''a Python script that fetches a url'''
+"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+
+
 import urllib.request
+
+
 if __name__ == '__main__':
     with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
         content = response.read()
